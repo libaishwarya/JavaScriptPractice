5ff3980fce9171a0463062de08555f5b8e0c5cd6
libaishwarya <libaishwarya@gmail.com>
2024-03-03 12:24:19 +0530

    practiced setTimeout

--- setTimout.js
+++ setTimout.js
@@ -1,9 +1,22 @@
-function showTime() {
-    let dateTime= new Date();
-    let time = dateTime.toLocaleTimeString();
-    console.log(time)
-     setTimeout(showTime, 3000);
-}
-showTime();
+// function showTime() {
+//     let dateTime= new Date();
+//     let time = dateTime.toLocaleTimeString();
+//     console.log(time)
+//      setTimeout(showTime, 3000);
+// }
+// showTime();
 
-// This program runs indefinitely (until the memory runs out).
+// This program runs indefinitely (until the memory runs out)
+
+
+
+function greet() {
+    console.log('Hello world');
+}
+
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+setTimeout(greet, 2000);
+sayName('John');
