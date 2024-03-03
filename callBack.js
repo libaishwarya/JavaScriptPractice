ea28b1a6113186be3c9c68017309744ff1460408
libaishwarya <libaishwarya@gmail.com>
2024-03-03 12:30:58 +0530

    basic implementation of callBack in JS

---
+++ callBack.js
@@ -0,0 +1,10 @@
+function greet(name, callback) {
+    console.log('Hello world');
+    callback(name);
+}
+
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+setTimeout(greet, 2000, 'John', sayName);
